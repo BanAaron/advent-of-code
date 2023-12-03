9a39cd7a383ee53cb0f26e43c5de33ea0f5c33bd
aarontbarratt <aarontbarratt@gmail.com>
2023-12-03 13:40:15 +0000

    some of the worst code I have ever written

--- 2023/03/part_one.py
+++ 2023/03/part_one.py
@@ -1,9 +1,21 @@
-def check_adjacent(x: int, y: int) -> bool:
-    # cardinal
-    # up
-    pass
+def check_adjacent(matrix: list[list], x: int, y: int) -> list:
+    directions = []
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if (i, j) != (0, 0):
+                directions.append((i, j))
 
-    # diagonal
+    neighbors = []
+    for dx, dy in directions:
+        new_x, new_y = x + dx, y + dy
+        if 0 <= new_x < len(matrix) and 0 <= new_y < len(matrix[0]):
+            neighbors.append(matrix[new_x][new_y])
+
+    return neighbors
+
+
+def contains_symbol(lst: list) -> bool:
+    return any(character not in '.0123456789' for character in lst)
 
 
 if __name__ == '__main__':
@@ -22,18 +34,28 @@ if __name__ == '__main__':
         for y, c in enumerate(r):
             arr[x][y] = c
 
-    print(data, "\n")
-
     numbers: list[int] = []
     store: str = ""
+    adjacent: list[bool] = []
 
     for x in range(height):
+        # edge case when the number ends at the end of a line
+        if store.isdigit() and contains_symbol(adjacent):
+            numbers.append(int(store))
+            store = ""
+            adjacent = []
+
         for y in range(width):
             char = arr[x][y]
             if char.isdigit():
                 store += char
-            elif len(store) > 1:
+                adjacent += check_adjacent(arr, x, y)
+            elif store.isdigit() and contains_symbol(adjacent):
                 numbers.append(int(store))
                 store = ""
+                adjacent = []
+            else:
+                store = ""
+                adjacent = []
 
-    print(numbers)
+    print(sum(numbers))
