1f6318b2d78b15d46708cf3708c9a3e0cad5eb3f
Khephren <aarontbarratt@gmail.com>
2023-12-04 12:59:15 +0000

    day 4 part 1 python

---
+++ 2023/04/part_one.py
@@ -0,0 +1,23 @@
+from math import floor
+
+with open("data", "r") as file:
+    data = file.read()
+    cards = data.splitlines()
+
+scores: list[int] = []
+
+for card in cards:
+    card = card.split(":")
+    numbers = card[1].split("|")
+    winning_numbers = numbers[0].split()
+    my_numbers = numbers[1].split()
+
+    count = -1
+    for i, n in enumerate(my_numbers):
+        if n in winning_numbers:
+            count += 1
+
+    score = floor(2 ** count)
+    scores.append(score)
+
+print(sum(scores))
